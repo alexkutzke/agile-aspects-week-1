23986e33be95219d8410c4018e62cbd9f20e17dd
Renata Soares <renatasoares@MacBook-Pro-de-Renata.local>
2023-08-05 16:24:13 -0300

    ïnicial

--- allergies.js
+++ allergies.js
@@ -3,7 +3,26 @@
 // convenience to get you started writing code faster.
 //
 
+/*class Alergia {
+  cod;
+  nome;
+  constructor(cod, nome) {
+    this.cod = cod;
+    this.nome = nome;
+  }
+}*/
 export class Allergies {
+
+  alergias = [
+    {cod: 1, nome: 'eggs'},
+    {cod: 2, nome: 'peanuts'},
+    {cod: 4, nome: 'shellfish'},
+    {cod: 8, nome: 'strawberries'},
+    {cod: 16, nome: 'tomatoes'},
+    {cod: 32, nome: 'chocolate'},
+    {cod: 64, nome: 'pollen'},
+    {cod: 128, nome: 'cats'}
+  ]
   constructor() {
     throw new Error('Remove this statement and implement this function');
   }
