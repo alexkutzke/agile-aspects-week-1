7a6ce3a3acd891f7974b9c1dfe3e194bcaf9d2d2
Rahula Palu Caleffi <rahula_dd@hotmail.com>
2023-08-08 19:46:54 -0300

    fixed allergyList array order to pass multiple check

--- allergies.js
+++ allergies.js
@@ -2,15 +2,16 @@
 
 export class Allergies {
   constructor(score) {
+    console.log("score", this.score);
     this.allergyList = [
-      { name: "cats", score: 128 },
-      { name: "pollen", score: 64 },
-      { name: "chocolate", score: 32 },
-      { name: "tomatoes", score: 16 },
-      { name: "strawberries", score: 8 },
-      { name: "shellfish", score: 4 },
-      { name: "peanuts", score: 2 },
       { name: "eggs", score: 1 },
+      { name: "peanuts", score: 2 },
+      { name: "shellfish", score: 4 },
+      { name: "strawberries", score: 8 },
+      { name: "tomatoes", score: 16 },
+      { name: "chocolate", score: 32 },
+      { name: "pollen", score: 64 },
+      { name: "cats", score: 128 },
     ];
 
     this.score = score;
