a71a0f7e2a29e1087746a6a4b7a09397f42bd589
jefersonthimoteo <jeferson.thimoteo@mega.com.br>
2023-08-12 09:35:09 -0300

    Exercício aspectos ágeis de programação

--- allergies.js
+++ allergies.js
@@ -1,19 +1,32 @@
 //
-// This is only a SKELETON file for the 'Allergies' exercise. It's been provided as a
+// This is only a SKELETON file for the "Allergies" exercise. It"s been provided as a
 // convenience to get you started writing code faster.
 //
 
+const allergies = {
+  1: "eggs", 
+  2: "peanuts", 
+  4: "shellfish", 
+  8: "strawberries",
+  16: "tomatoes", 
+  32: "chocolate", 
+  64: "pollen", 
+  128: "cats"};
+
 export class Allergies {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
+  allergiesBitwise = 0;
+  constructor(allergiesBitwise) {
+    this.allergiesBitwise = allergiesBitwise;
   }
 
   list() {
-    throw new Error('Remove this statement and implement this function');
+    return Object.keys(allergies)
+      .filter(allergyKey => this.allergiesBitwise & allergyKey)
+      .map(i => allergies[i]);
   }
 
-  allergicTo() {
-    throw new Error('Remove this statement and implement this function');
+  allergicTo(allergyName) {
+    return this.list().includes(allergyName);
   }
 }
 
